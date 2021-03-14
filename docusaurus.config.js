1458001d9d906955091258bc322b9113469b03b4
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-03-14 06:33:15 -0400

    Convert to the theme created by react-native as i like it better

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,9 +11,10 @@ module.exports = {
   projectName: 'Website',
   themeConfig: {
     navbar: {
+	  style: 'dark',
       title: 'MogAmp',
       logo: {
-        alt: 'Logo',
+        alt: 'MogAmp',
         src: '/logo.png',
       },
       items: [
@@ -82,18 +83,38 @@ module.exports = {
 	colorMode: {
       defaultMode: 'dark'
 	},
+	metadatas: [
+      {
+        property: 'og:image',
+        content: 'https://mogamp.com/logo.png',
+      },
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {
+        name: 'twitter:image',
+        content: 'https://mogamp.com/logo.png',
+      },
+      {name: 'twitter:site', content: '@MoogleTroupe'},
+    ],
   },
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
+		  showLastUpdateAuthor: false,
+          showLastUpdateTime: true,
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/MogAmp/Website/edit/main/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          //customCss: require.resolve('./src/css/custom.css'),
+		  customCss: [
+            require.resolve('./src/css/customTheme.scss'),
+          //  require.resolve('./src/css/index.scss'),
+          //  require.resolve('./src/css/showcase.scss'),
+          //  require.resolve('./src/css/versions.scss'),
+          ],
         },
       },
     ],
@@ -104,23 +125,20 @@ module.exports = {
 	localeConfigs: {
       en: {
         label: 'English',
-        direction: 'ltr',
       },
 //	  fr: {
 //        label: 'Français',
-//        direction: 'ltr',
 //      },
 //      ja: {
 //        label: '日本語',
-//        direction: 'ltr',
 //      },
       ko: {
         label: '한국어',
-        direction: 'ltr',
       },
     },
   },
   plugins: [
+    'docusaurus-plugin-sass',
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       {
